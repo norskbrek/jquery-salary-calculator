9b694996158bc3e15f73863dfa11ddf4a380ab2c
Andy Rollins <snillorydna@gmail.com>
2019-04-14 13:20:09 -0500

    tables functional / submit button functional

--- salary-calc-client.js
+++ salary-calc-client.js
@@ -1,8 +1,8 @@
 // initialize employee array @ empty
 let employees = [
-    {firstName: 'Derek', lastName: 'Chunsler', id: '123', jobTitle: 'Baker', annualSalary: 14400},
-    {firstName: 'Brenda', lastName: 'Lipsley', id: '456', jobTitle: 'Cook', annualSalary: 24000},
-    { firstName: 'Zaphyre', lastName: 'McPripps', id: '789', jobTitle: 'Soup&ccedilon', annualSalary: 36000}
+    // {firstName: 'Derek', lastName: 'Chunsler', id: '123', jobTitle: 'Baker', annualSalary: 14400},
+    // {firstName: 'Brenda', lastName: 'Lipsley', id: '456', jobTitle: 'Cook', annualSalary: 24000},
+    // {firstName: 'Zaphyre', lastName: 'McPripps', id: '789', jobTitle: 'Soup&ccedilon', annualSalary: 36000}
 ];
 let monthlyCosts = 0;
 
@@ -10,10 +10,11 @@ $(document).ready(readyNow);
 
 // readyNow function (at top; other functions - alphabetical)
 function readyNow() {
+    console.log('We are ready.');
+    
     $('#submitButton').on('click', addEmployee);
-    $('.deleteButton').on('click', deleteEmployee);
         
-    displayEmployees(employees);
+    // displayEmployees(employees);
     calculateMonthlyCosts(employees);
 }
 
@@ -31,12 +32,6 @@ function addEmployee() {
     } 
     // push object into employee array
     employees.push(newEmployee);
-    
-    // display employee on DOM
-    displayEmployees(employees);
-    
-    // calculate monthly costs
-    calculateMonthlyCosts(employees);
 
     // clear inputs 
     $('#firstNameIn').val('');
@@ -45,65 +40,63 @@ function addEmployee() {
     $('#jobTitleIn').val('');
     $('#annualSalaryIn').val('');
 
+    // display employee on DOM
+    displayEmployees();
+
+    // calculate monthly costs
+    calculateMonthlyCosts(employees);
+
 } // end addEmployee
 
 function calculateMonthlyCosts(employees) {
     let annualCosts = 0;
-
     for ( let i = 0; i < employees.length; i++) {
         annualCosts = annualCosts + Number(employees[i].annualSalary);
     }
-
     // set new monthlyCosts
     monthlyCosts = (annualCosts / 12).toFixed(2);
-
     // display on DOM
     displayMonthlyCosts();
-
 } // end calculateMonthlyCosts
 
 function deleteEmployee() {
-    
     console.log('In deleteEmployee:');
-
-    // let removeId = employees.map(employees.id === $('#removeIdIn'));
-    // employees.splice(removeId, 1);
-
-    // console.log('removeId:', removeId);
     
-    // loop through employees
-    // if current employee object's id matches
-    // then remove employee at index
+    // let removeId = thisEmployee.parent().id;
+    // //loop through employees
+    // //if current employee object's id matches
+    // //then remove employee at index
     // for (let i = 0; i < employees.length; i++) {
-    //     if (employees[i].id === removeId) {
+    //     if (removeId === employees[i].id) {
     //         employees[i].splice(i, 1);
         
     //     }
-    //     return employees;
+       
     // }
 
-    // displayEmployees(employees);
+    // thisEmployee.remove();
+
+    //displayEmployees(employees);
 } // end deleteEmployee
 
-function displayEmployees(employees) {
+function displayEmployees() {
 
-    // --- OLD FUNCTION --- EMPLOYEE LIST
-    // target ul element
-    let el = $('#employeeOut');
-    el.empty();
-    // loop through the employees
-    for (let i = 0; i < employees.length; i++) {
-        // display each item as a li: 
-        const listItem = `<li><b>Name:</b> ${employees[i].firstName} ${employees[i].lastName} 
-            <b>ID:</b> ${employees[i].id} 
-            <b>Title:</b> ${employees[i].jobTitle} 
-            <b>Annual Salary:</b> ${employees[i].annualSalary}
-            <button class="btn btn-danger deleteButton">Remove</button></li>`;
-        
-            el.append(listItem);
-    }
-    // let el = $('#employeeOut');
-    // el.empty();
+    let tableItem = employees[employees.length-1];
+    
+    $('#tableBody').append(`
+        <tr>
+            <td>${tableItem.firstName}</td>
+            <td>${tableItem.lastName}</td>
+            <td>${tableItem.id}</td>
+            <td>${tableItem.jobTitle}</td>
+            <td>${tableItem.annualSalary}</td>
+            <td><button id="#deleteButton">Delete</button></td>
+        </tr>`);
+
+    $('#deleteButton').on('click', function() {
+        deleteEmployee($(this));
+    });
+    
 
     // for (let i = 0; i < employees.length; i++) {
     //     const tableItem = `<tr><td>${employees[i].firstName}</td>
@@ -112,6 +105,8 @@ function displayEmployees(employees) {
     //         <td>${employees[i].jobTitle}</td>
     //         <td>${employees[i].annualSalary}</td>
     //     </tr>`;
+
+    //     el.append(tableItem);
 
     // }
 } // end displayEmployees
