322162b05ab27e92c3a6b4c3a67845a4d9e72f2a
Andy Rollins <snillorydna@gmail.com>
2019-04-14 14:03:27 -0500

    corrected typo - deleteButton now responsive

--- salary-calc-client.js
+++ salary-calc-client.js
@@ -13,14 +13,13 @@ function readyNow() {
     console.log('We are ready.');
     
     $('#submitButton').on('click', addEmployee);
-        
+    
     // displayEmployees(employees);
     calculateMonthlyCosts(employees);
 }
 
 function addEmployee() {
     console.log('In addEmployee:');
-    
     // user input
     // new object literal
     const newEmployee = {
@@ -62,21 +61,8 @@ function calculateMonthlyCosts(employees) {
 function deleteEmployee() {
     console.log('In deleteEmployee:');
     
-    // let removeId = thisEmployee.parent().id;
-    // //loop through employees
-    // //if current employee object's id matches
-    // //then remove employee at index
-    // for (let i = 0; i < employees.length; i++) {
-    //     if (removeId === employees[i].id) {
-    //         employees[i].splice(i, 1);
-        
-    //     }
-       
-    // }
 
-    // thisEmployee.remove();
 
-    //displayEmployees(employees);
 } // end deleteEmployee
 
 function displayEmployees() {
@@ -90,25 +76,12 @@ function displayEmployees() {
             <td>${tableItem.id}</td>
             <td>${tableItem.jobTitle}</td>
             <td>${tableItem.annualSalary}</td>
-            <td><button id="#deleteButton">Delete</button></td>
+            <td><button id="deleteButton">Delete</button></td>
         </tr>`);
 
-    $('#deleteButton').on('click', function() {
-        deleteEmployee($(this));
-    });
-    
+    $('#tableBody').on('click', '#deleteButton', deleteEmployee);  
 
-    // for (let i = 0; i < employees.length; i++) {
-    //     const tableItem = `<tr><td>${employees[i].firstName}</td>
-    //         <td>${employees[i].lastName}</td>
-    //         <td>${employees[i].id}</td>
-    //         <td>${employees[i].jobTitle}</td>
-    //         <td>${employees[i].annualSalary}</td>
-    //     </tr>`;
 
-    //     el.append(tableItem);
-
-    // }
 } // end displayEmployees
 
 function displayMonthlyCosts() {
